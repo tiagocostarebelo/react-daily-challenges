b12891d0d06a807ed8606da639a525f9b6b6d18a
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-11-05 11:21:08 +0000

    feat: add basic controlled input forms for name and email
    Each input uses its own state to sync and display values dynamically.

--- beginner/daily-react-003-controlled-input-form/src/App.jsx
+++ beginner/daily-react-003-controlled-input-form/src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Controlled Input form</h1>
+      </header>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor='nameInput'>Your name here</label><br />
+          <input
+            type="text"
+            id="nameInput"
+            name="nameInput"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            defaultValue="Your name here"
+            required /><br />
+
+          <label htmlFor='emailInput'>Your email here</label><br />
+          <input
+            type="email"
+            id="emailInput"
+            name="emailInput"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            defaultValue="Your email here"
+            required /><br />
+          <button type="submit">Submit</button>
+        </form>
+        <div>
+          {name && <p>{name}</p>}
+          {email && <p>{email}</p>}
+        </div>
+      </main>
     </>
   )
 }
