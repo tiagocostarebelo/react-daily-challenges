e1665efea844b137e8de5422a990b15866a6fb8f
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-11-12 20:42:20 +0000

    feat: fixed some issues when creating the Accordion component, by moving state and logic inside the component. Added more data to the AccordionData, to create multiple accordions

--- intermediate/daily-react-008-accordion/src/App.jsx
+++ intermediate/daily-react-008-accordion/src/App.jsx
@@ -3,19 +3,41 @@ import Accordion from './components/Accordion';
 
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const accordionData = {
-    title: 'Section 1',
-    content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-      laborum cupiditate possimus labore, hic temporibus velit dicta earum
-      suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-      voluptatem.`
-  };
+  const accordionData = [
+    {
+      title: 'Section 1',
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore, hic temporibus velit dicta earum
+        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+        voluptatem.`
+    },
+    {
+      title: 'Section 2',
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore, hic temporibus velit dicta earum
+        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+        voluptatem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore, hic temporibus velit dicta earum
+        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+        voluptatem.`
+    },
+    {
+      title: 'Section 3',
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore, hic temporibus velit dicta earum
+        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
+        voluptatem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore`
+    },
+    {
+      title: 'Section 4',
+      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
+        laborum cupiditate possimus labore.`
+    }
+  ]
 
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
+
 
   return (
     <>
@@ -23,7 +45,13 @@ function App() {
         <h1>Accordion</h1>
       </header>
       <main>
-        <Accordion accordionData={accordionData} onClick={handleClick} isOpen={isOpen} />
+        {accordionData.map(({ title, content }) => (
+          <Accordion title={title} content={content} />
+        ))
+
+
+
+        }
       </main>
     </>
   )
--- intermediate/daily-react-008-accordion/src/components/Accordion.jsx
+++ intermediate/daily-react-008-accordion/src/components/Accordion.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 
-const Accordion = ({ accordionData, onClick, isOpen }) => {
+const Accordion = ({ title, content }) => {
+    const [isOpen, setIsOpen] = useState(false);
 
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className="accordion">
             <div className="accordion-item">
-                <div className="accordion-title" onClick={onClick}>
-                    <div>{accordionData.title}</div>
+                <div className="accordion-title" onClick={handleClick}>
+                    <div>{title}</div>
                     <div>+</div>
                 </div>
-                {isOpen && <div className="accordion-content">{accordionData.content}</div>}
+                {isOpen && <div className="accordion-content">{content}</div>}
             </div>
         </div>
     )
