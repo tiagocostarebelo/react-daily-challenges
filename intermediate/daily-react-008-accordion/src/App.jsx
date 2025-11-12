bc5152974c55a204e1603143dc91084cd7a9ba33
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-11-12 20:48:09 +0000

    feat: cleaned code a bit more.
    Moved accordionData to a file of its own and imported it in the App.jsx;

--- intermediate/daily-react-008-accordion/src/App.jsx
+++ intermediate/daily-react-008-accordion/src/App.jsx
@@ -1,43 +1,9 @@
 import { useState } from 'react';
 import Accordion from './components/Accordion';
+import { accordionData } from './data/data.js';
 
 
 function App() {
-
-  const accordionData = [
-    {
-      title: 'Section 1',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem.`
-    },
-    {
-      title: 'Section 2',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem.`
-    },
-    {
-      title: 'Section 3',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore, hic temporibus velit dicta earum
-        suscipit commodi eum enim atque at? Et perspiciatis dolore iure
-        voluptatem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore`
-    },
-    {
-      title: 'Section 4',
-      content: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis sapiente
-        laborum cupiditate possimus labore.`
-    }
-  ]
-
-
 
   return (
     <>
@@ -48,9 +14,6 @@ function App() {
         {accordionData.map(({ title, content }) => (
           <Accordion title={title} content={content} />
         ))
-
-
-
         }
       </main>
     </>
