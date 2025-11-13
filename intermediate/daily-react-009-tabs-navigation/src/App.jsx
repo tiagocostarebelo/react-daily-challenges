6dc4e66333a82f0aaaab2d80e57011582cc30d16
Tiago Costa Rebelo <costarebelo.tiago@gmail.com>
2025-11-13 14:21:21 +0000

    fixed an issue with the className conditional rendering

--- intermediate/daily-react-009-tabs-navigation/src/App.jsx
+++ intermediate/daily-react-009-tabs-navigation/src/App.jsx
@@ -15,12 +15,12 @@ function App() {
       </header>
       <main>
         <header>
-          {tabsData.map(({ title, content }, index) => (
-            <div key={index} onClick={() => handleTabClick(index)}>{title}</div>
+          {tabsData.map(({ title }, index) => (
+            <div className={`tab ${isTabActive === title ? "active" : ""}`} key={index} onClick={() => handleTabClick(index)}>{title}</div>
           ))}
         </header>
         <div>
-          <p>Text renders here according to the active tab title. Defaults to English.</p>
+
         </div>
       </main>
     </>
